1ab53132e818446b91983e8ac4a6262d7f0cd4a2
zyuseoi <67495236+zyuseoi@users.noreply.github.com>
2021-08-07 17:26:47 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -8,12 +8,13 @@ git clone https://github.com/liuran001/openwrt-packages
 sed -i 's#192.168.1.1#192.168.2.101#g' package/base-files/files/bin/config_generate
 # 自定义取消密码
 sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' package/lean/default-settings/files/zzz-default-settings
-# 自定义主机名
-sed -i 's#ImmortalWrt/OpenWrt#g' package/base-files/files/bin/config_generate
 
 # 自定义配置文件
 rm -f ./.config*
 touch ./.config
+
+# 自定义主机名
+sed -i 's#ImmortalWrt/OpenWrt#g' package/base-files/files/bin/config_generate
 
 # 自定义配置
 cat >> .config <<EOF
