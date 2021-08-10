33d8c6368f7cf68accd5ee666a4601b9a50f3dba
zyuseoi <67495236+zyuseoi@users.noreply.github.com>
2021-08-10 18:56:50 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -16,3 +16,4 @@ sed -i 's#ImmortalWrt/OpenWrt#g' package/base-files/files/bin/config_generate
 sed -i 's#192.168.1.1#192.168.2.101#g' package/base-files/files/bin/config_generate
 # 自定义取消密码
 sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' package/lean/default-settings/files/zzz-default-settings
+
